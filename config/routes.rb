454b328f5faa2d0218110eced375e7463fd41e1f
yarzin <zinyaroslav@gmail.com>
2016-06-13 19:19:53 +0300

    create friendship

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :friendships
   resources :products, only: [:index, :new, :create, :edit, :update]
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610093639) do
+ActiveRecord::Schema.define(version: 20160613160720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160610093639) do
   end
 
   add_index "carts", ["account_id"], name: "index_carts_on_account_id", using: :btree
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "account_id"
+    t.integer  "friend_id"
+    t.string   "create"
+    t.string   "destroy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
