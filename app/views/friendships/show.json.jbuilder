3ce5817b3af4117612951d25d523977be47fc206
yarzin <zinyaroslav@gmail.com>
2016-06-13 12:40:01 +0300

    some changes whith bd

--- app/views/friendships/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @friendship, :id, :Accounts_id, :friend_id, :create, :destroy, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :friendships
   resources :products, only: [:index, :new, :create, :edit, :update]
 
 
