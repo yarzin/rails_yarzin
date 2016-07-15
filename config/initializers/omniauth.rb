c9bd4c4dbe47e39e1a7707066ca306f616abb558
yarzin <zinyaroslav@gmail.com>
2016-07-15 18:48:30 +0300

    some fix

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "984347758876-h0g49gl7lef83ju3nau6hbpraibdn32g.apps.googleusercontent.com", "bPvO47xtEy8Yygs7oaX3FFG5"
+end
